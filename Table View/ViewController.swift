8feefe3e240a251642ef07bca453014f3bb60e31
rasyidrw <rasyidrespati.30@gmail.com>
2020-01-28 11:21:40 +0700

    Initial Commit

---
+++ Table View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table View
+//
+//  Created by Rasyid Respati Wiriaatmaja on 28/01/20.
+//  Copyright © 2020 rasyidrw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
